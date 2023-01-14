c7ab7e78eb09352e01d8d06649a517818e51669a
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-14 23:25:39 +0100

    person

---
+++ src/main/java/com/loveable/Person.java
@@ -0,0 +1,11 @@
+package com.loveable;
+
+public abstract class Person {
+    private String name;
+    private int age;
+
+    public void talk() {
+        System.out.println("Hi Faith2");
+    }
+
+}
