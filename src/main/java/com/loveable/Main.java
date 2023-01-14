03d919aff3bd027784a34738dfd423a04d047440
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-14 23:37:45 +0100

    Clean up

--- src/main/java/com/loveable/Main.java
+++ src/main/java/com/loveable/Main.java
@@ -1,22 +1,7 @@
 package com.loveable;
 
-import com.loveable.arrayList.ArrayListDemo;
-import com.loveable.linkedList.LinkedListDemo;
-
 public class Main {
-
-
     public static void main(String[] args) {
-        Boss faith = new Boss();
 
-        Person aruby = new Person() {
-            @Override
-            String sing() {
-                return "hehhe";
-            }
-        };
-
-        System.out.println(faith.sing());
-        System.out.println(aruby.sing());
     }
 }
