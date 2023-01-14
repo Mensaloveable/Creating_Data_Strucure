796fbd346d107b1c0ca35b1a10b0fc8765098c64
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-01-14 23:32:15 +0100

    Boss

--- src/main/java/com/loveable/Main.java
+++ src/main/java/com/loveable/Main.java
@@ -5,4 +5,11 @@ import com.loveable.linkedList.LinkedListDemo;
 
 public class Main {
     Boss faith = new Boss();
+
+    Person aruby = new Person() {
+        @Override
+        void sing() {
+            System.out.println("hehehe");
+        }
+    };
 }
